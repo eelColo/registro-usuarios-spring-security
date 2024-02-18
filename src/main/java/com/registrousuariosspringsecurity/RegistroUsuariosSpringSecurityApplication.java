b7a913aa0b933af0661240081c1bfd3724630010
Franco <francolens2002@gmail.com>
2024-02-18 11:51:26 -0300

    Entities junto con su relacion entre si

---
+++ src/main/java/com/registrousuariosspringsecurity/RegistroUsuariosSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.registrousuariosspringsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistroUsuariosSpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistroUsuariosSpringSecurityApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/registrousuariosspringsecurity/entities/Rol.java
@@ -0,0 +1,39 @@
+package com.registrousuariosspringsecurity.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "rol")
+public class Rol {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String rol;
+
+    public Rol() {
+    }
+
+    public Rol(Long id, String rol) {
+        this.id = id;
+        this.rol = rol;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getRol() {
+        return rol;
+    }
+
+    public void setRol(String rol) {
+        this.rol = rol;
+    }
+}
